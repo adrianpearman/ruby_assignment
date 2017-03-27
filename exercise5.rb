3aae00cd6e49cce9095691644e8a8471fb6eea91
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-27 15:30:31 -0400

    finish exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,7 @@
+puts "How many pizzas are your ordering?"
+  pizzaquantity = gets.to_i.round
+
+puts "How many toppings are you ordering?"
+  toppingsquantity = gets.to_i.round
+
+puts "You are ordering #{pizzaquantity} pizzas with #{toppingsquantity} toppings."
