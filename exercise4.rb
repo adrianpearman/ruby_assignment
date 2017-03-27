d0477c0a85bd402d96b013a9649c3978213c9bb6
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-27 14:52:14 -0400

    finish exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,1 +1,18 @@
-cat > exercise5.rb
+counter = 0
+
+while counter < 100
+      counter += 1
+        if
+          (counter % 3 == 0) && (counter % 5 == 0)
+          puts "bitmaker"
+      elsif
+         counter % 5 == 0
+          puts "maker"
+
+      elsif
+         counter % 3 == 0
+          puts "bit"
+      else
+        puts counter
+      end
+    end
