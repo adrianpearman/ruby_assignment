754911667bda4736039b2cb4aa874252933a8ab8
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-27 17:06:48 -0400

    finsih exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,10 +1,34 @@
-distance = 0
-
 puts "Are you going for a run or walk today?"
-  exercise = gets
+  exercise = gets.chomp
+
+puts "How far are you going from home?"
+    distance = gets.to_f
+    if exercise == "run"
+      print "You are going to exercise for #{distance += 5} km. \n"
+    elsif exercise == "walk"
+      print "You are going to exercise for #{distance += 1} km. \n"
+    end
+
+puts "How much energy do you have?"
+    energy = gets.to_i
+    if energy == 0
+      print "You're exhausted, go home and rest!  \n"
+    else
+      print "Keep going!  \n"
+    end
+
+puts "Are you ready to go home?"
+  response = gets.chomp
+    if response == "yes"
+      print "You can go home now \n"
+    else
+      print "Keep going! \n"
+    end
+
+
 if exercise == "run"
-  print "ok"
+  newenergy = energy - 1
+else
+  newenergy = energy + 1
 end
-if exercise == "walk"
-  print "really?"
-end
+puts "After #{distance} km, you now have units #{newenergy}."
