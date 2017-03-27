ed2ae9eca5de91192724881390cb5acaf83403fb
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-27 13:28:21 -0400

    finish exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
 puts "What is your name?"
+name = gets
+puts "Hello #{name}"
+
+puts "No but what is your real name"
+name1 = gets.chomp
+puts "Oh okay, sorry #{name1}"
+
+puts "How old are you?"
+age = gets
+puts "#{age}, i didnt think you were that young"
