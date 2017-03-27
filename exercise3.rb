83ac4a676aec40e6e014182ae9dc5096a0addd6d
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-27 15:45:00 -0400

    refinish exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -8,4 +8,4 @@ puts "Oh okay, sorry #{realname}"
 
 puts "How old are you?"
 age = gets.chomp
-puts "#{realname} also known as #{nickname}you are #{age} years old, which means you were born in the year #{2017 - age.to_i}"
+puts "#{realname} also known as #{nickname} you are #{age} years old, which means you were born in the year #{2017 - age.to_i}"
