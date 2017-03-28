24c03fee919deeb3c6f2f2fabcf4145f96873318
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-28 16:49:06 -0400

    revised exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,11 @@
-puts "Are you going for a run or walk today?"
+puts "Are you going for a Run(R) or Walk(W) today?"
   exercise = gets.chomp
 
 puts "How far are you going from home?"
     distance = gets.to_f
-    if exercise == "run"
+    if exercise.downcase.start_with?("r")
       print "You are going to exercise for #{distance += 5} km. \n"
-    elsif exercise == "walk"
+    elsif exercise.downcase.start_with?("w")
       print "You are going to exercise for #{distance += 1} km. \n"
     end
 
@@ -17,9 +17,9 @@ puts "How much energy do you have?"
       print "Keep going!  \n"
     end
 
-puts "Are you ready to go home?"
+puts "Are you ready to go home; Yes(Y) or No(N)?"
   response = gets.chomp
-    if response == "yes"
+    if response.downcase.start_with?("y")
       print "You can go home now \n"
     else
       print "Keep going! \n"
@@ -31,4 +31,4 @@ if exercise == "run"
 else
   newenergy = energy + 1
 end
-puts "After #{distance} km, you now have units #{newenergy}."
+puts "After #{distance} km, you now have #{newenergy} units of energy."
